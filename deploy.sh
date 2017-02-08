522547975df0a1679b8fb8b5ef77ee4222a92c6c
meza <meza@meza.hu>
2017-02-08 20:23:55 +0000

    fix env variable referencing

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export RELEASE=true
-./gradlew curseforge -Pcurseforge_key=${curseforge_key} -Prelease_type="$1" -Ptag_name="$2" -Pbuild_number=$3
+./gradlew curseforge -Pcurseforge_key="$curseforge_key" -Prelease_type="$1" -Ptag_name="$2" -Pbuild_number=$3
