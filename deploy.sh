3b54e6132dd2fff6dd2c5a6d85f0b297a21a8c39
meza <meza@meza.hu>
2017-02-08 19:56:18 +0000

    maybe now?

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 RELEASE = true
-./gradlew curseforge -Pcurseforge_key=${curseforge_key} -Prelease_type="$1" -Ptag_name="$2"
+./gradlew curseforge -Pcurseforge_key=${curseforge_key} -Prelease_type="$1" -Ptag_name="$2" -Pbuild_number=$3
