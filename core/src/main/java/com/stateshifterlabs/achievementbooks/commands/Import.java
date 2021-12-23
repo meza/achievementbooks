a48628cbd0fa1a8126b19768b46f78b444d81b54
meza <meza@meza.hu>
2021-12-23 20:37:55 +0000

    chore: the great reset

--- core/src/main/java/com/stateshifterlabs/achievementbooks/commands/Import.java
+++
@@ -1,8 +0,0 @@
-package com.stateshifterlabs.achievementbooks.commands;
-
-public class Import {
-
-	public static final int PERMISSION = 4;
-	public static final String NAME = "import";
-	public static final String USAGE = "import";
-}
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export RELEASE=true
-./gradlew curseforge -Pcurseforge_key="$curseforge_key" -Prelease_type="$1" -Ptag_name="$2" -Pbuild_number=$3
